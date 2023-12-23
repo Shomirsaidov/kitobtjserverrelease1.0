1bf17018a433b207001601e04d3a7975d7f95bd5
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2023-12-23 18:25:53 +0500

    cors stuff

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ app.use((req,res,next) => {
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Methods", "*");
+    res.set("Access-Control-Allow-Headers", "*");
     next();
   });
 
