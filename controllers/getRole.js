4e50cae5241b1b7f6f75598f949297b17d9f840b
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2023-12-23 18:09:38 +0500

    Update getRole.js

--- controllers/getRole.js
+++ controllers/getRole.js
@@ -3,8 +3,9 @@ const query = require('../conn.js')
 const getRole = (req,res) => {
     query("SELECT `username` FROM `clients` WHERE `token` = '" + req.user_token + "'")
         .then(r => {
+            console.log("SELECT `username` FROM `clients` WHERE `token` = '" + req.user_token + "'")
             console.log(r)
-            res.send({role: r})
+            res.send({role: r[0].username})
         })
         .catch(e => {
             console.warn("GO fuck yourself !")
