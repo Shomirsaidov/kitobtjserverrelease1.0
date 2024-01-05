1a914f9fc89c1e13368da849f5f306038b683008
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2024-01-05 23:48:55 +0500

    Update getRole.js

--- controllers/getRole.js
+++ controllers/getRole.js
@@ -1,7 +1,7 @@
 const query = require('../conn.js')
 
 const getRole = (req,res) => {
-    query("SELECT `username` FROM `clients` WHERE `token` = '" + req.user_token + "'")
+    query("SELECT `username` FROM `clients` WHERE `token` = '" + req.body.user_token + "'")
         .then(r => {
             console.log("SELECT `username` FROM `clients` WHERE `token` = '" + req.body.user_token + "'")
             console.log(r)
